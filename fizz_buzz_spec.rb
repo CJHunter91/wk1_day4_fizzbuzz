14111b9b2c6b5f3a124990cfe127fa2eda6b6447
Chris Hunter <orkem2@gmail.com>
2017-06-29 11:07:54 +0100

    Added divisble_by_5 test function and extra divisible by 3 test

--- fizz_buzz_spec.rb
+++ fizz_buzz_spec.rb
@@ -8,9 +8,15 @@ require_relative('./fizz_buzz.rb')
 class FizzBuzzSpec < MiniTest::Test
 
   def test_divisible_by_3()
-    expected = "Fizz"
-    actual = fizz_buzz(3)
-    assert_equal(expected, actual)
+    expected = "Fizz" 
+    assert_equal(expected, fizz_buzz(3))
+    assert_equal(expected, fizz_buzz(9))
+    assert_equal(expected, fizz_buzz(15))
   end
 
+  def test_divisible_by_5()
+    expected = "Buzz"
+    assert_equal(expected, fizz_buzz(5))
+    assert_equal(expected, fizz_buzz(10))
+    assert_equal(expected, fizz_buzz(15))
 end
