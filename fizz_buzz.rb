e539b029d7ccca44eac3b43a4855d1f0cccd1012
Chris Hunter <orkem2@gmail.com>
2017-06-29 11:26:13 +0100

    Removed floating point numbers as was irrelevant

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,8 +1,8 @@
 # A function that will retrun 'Fizz', 'Buzz' or 'FizzBuzz' depending on input.
 
 def fizz_buzz(number)
-  return "FizzBuzz" if(number % 3.0 == 0) && (number % 5.0 == 0)
-  return "Fizz" if(number % 3.0 == 0)
-  return "Buzz" if(number % 5.0 == 0)
+  return "FizzBuzz" if(number % 3 == 0) && (number % 5 == 0)
+  return "Fizz" if(number % 3 == 0)
+  return "Buzz" if(number % 5 == 0)
   return number.to_s
 end
