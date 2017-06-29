211e6d5b1fbbc6c0204b073cb0533cb657d5a17b
Chris Hunter <orkem2@gmail.com>
2017-06-29 10:50:14 +0100

    Added divisible_by_3 test

---
+++ fizz_buzz.rb
@@ -0,0 +1,2 @@
+# A function that will retrun 'Fizz', 'Buzz' or 'FizzBuzz' depending on input.
+
--- fizz_buzz_spec.rb
+++ fizz_buzz_spec.rb
@@ -1,2 +1,16 @@
-require(Minitest/autostart)
-require(Minitest/rg)
+require('minitest/autorun')
+require('minitest/rg')
+
+require_relative('./fizz_buzz.rb')
+
+# A group of test to create a fizz_buzz function.
+
+class FizzBuzzSpec < MiniTest::Test
+
+  def divisible_by_3()
+    expected = "Fizz"
+    actual = fizz_buzz(3)
+    assert_equal(expected, actual)
+  end
+
+end
